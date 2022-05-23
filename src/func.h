173fc52bbf5fbfc328a2e0d6e71992f79188a743
xmu310 <xmu310@gmail.com>
2022-05-23 23:52:49 +0800

    some changes

--- src/func.h
+++ src/func.h
@@ -2,6 +2,5 @@
 #include"def.h"
 
 int64_t set_game(int64_t player_num);
-void init_card();
 void shuffling(sPile *pile,int64_t card_num);
 void end_game();
--- src/shuffling.c
+++ src/shuffling.c
@@ -5,7 +5,9 @@
 void shuffling(sPile *pile,int64_t card_num){
 	srand(time(0));
 	for(int i=0;i<card_num;i++){
-		int64_t x=rand()%card_num,y=rand()%card_num;
+		int64_t x,y;
+		x=rand()%card_num;
+		while((y=rand()%card_num)==x)1;
 		sPile tmp=pile[x];
 		pile[x]=pile[y];
 		pile[y]=tmp;
