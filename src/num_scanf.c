a1d17e3a64a890a3a1f449ad2fd6d742af27aca7
xmu310 <xmu310@gmail.com>
2022-06-02 18:57:14 +0800

    some changes

--- src/num_scanf.c
+++ src/num_scanf.c
@@ -6,7 +6,7 @@ int32_t num_scanf(int32_t *num){
 	int32_t re;
 	char buf[10000]={0},*endp;
 	scanf("%[^\n]",buf);
-	scanf("\n");
+	scanf("%*c");
 	*num=strtol(buf,&endp,10);
 	if(!strlen(buf)||*endp)re=0;else re=1;
 	return re;
