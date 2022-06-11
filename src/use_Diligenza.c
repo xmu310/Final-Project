0b59092d8d28a72120038e4da41b30f7b998f657
xmu310 <xmu310@gmail.com>
2022-06-12 05:10:24 +0800

    some changes

--- src/use_Diligenza.c
+++ src/use_Diligenza.c
@@ -1,4 +1,9 @@
 #include"func.h"
 
 void use_Diligenza(int32_t card_index){
+	get_stock(PlayerNow);
+	printf("Player%d got a card from stock pile.\n",PlayerNow+1);
+	get_stock(PlayerNow);
+	printf("Player%d got a card from stock pile.\n",PlayerNow+1);
+	get_card(&discard,&player[PlayerNow].hand,card_index);
 }
