df09aa32825900b636baebfb5ef3606644d82f88
xmu310 <xmu310@gmail.com>
2022-06-13 20:06:19 +0800

    some changes

--- src/player_dead.c
+++ src/player_dead.c
@@ -2,6 +2,11 @@
 
 int32_t player_dead(int32_t player_index){
 	if(!is_player_exist(player_index))return 0;
+	combine_pile(&player[player_index].hand,&player[player_index].equip);
+	for(int i=0;i<PlayerNum;i++){
+		if(player[i].role==Vulture_Sam)combine_pile(&player[i].hand,&player[player_index].hand);
+	}
+	combine_pile(&discard,&player[player_index].hand);
 	player[player_index].alive=0;
 	if(player[player_index].iden==Sheriff)SheriffAlive--;
 	if(player[player_index].iden==Deputy_Sheriff)DeputySheriffAlive--;
