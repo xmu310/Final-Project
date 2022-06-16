cad6d2908e9f6e4d5d3797d6968df9b8dc19f496
antingchao <98510877+antingchao@users.noreply.github.com>
2022-06-17 00:27:14 +0800

    Update det_Jail.c

--- src/det_Jail.c
+++ src/det_Jail.c
@@ -27,7 +27,7 @@ int32_t det_Jail(){
         return 2;
     }else{ // still in jail, return 1
         print_ascii(aJail);
-        printf("player%d is still in the Jail :(\n", PlayerNow);
+        printf("Not Heart: player%d is still in the Jail :(\n", PlayerNow);
         get_last_card(&discard, &temp); // discard determine card
         return 1;
     }    
